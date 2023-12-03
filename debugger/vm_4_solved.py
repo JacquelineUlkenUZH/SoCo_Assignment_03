ac84d70bbb35984eabbcc0a4ad2ce72611289a0f
luzmediach <marc@luzmedia.ch>
2023-12-03 11:12:33 +0100

    4.2 solved

--- debugger/vm_4_solved.py
+++ debugger/vm_4_solved.py
@@ -19,34 +19,66 @@ class VirtualMachine4solved(VirtualMachineBreak):
                 elif command not in self.handlers:
                     self.write(f"Unknown command {command}")
                 # Functions with 2 arguments (self, addr) from "old" parent classes
+                # to keep old functionality, as in the book
                 elif len(inspect.signature(self.handlers[command]).parameters) == 1:
                     interacting = self.handlers[command](self.ip)
                 # New functions with 3+ arguments (self, addr, *cargs)
                 else:
-                    interacting = self.handlers[command](self.ip, *cargs)
+                    valid_cargs, cargs = self._cargs_to_addresses(cargs)
+                    if valid_cargs:
+                        interacting = self.handlers[command](self.ip, *cargs)
+                    else:
+                        self.write(f"Invalid memory address")
             except EOFError:
                 self.state = VMState.FINISHED
                 interacting = False
     # [/interact]
     
-    def show_memory(self, start, end=None):
-        if end is None: end = start # note end+1 in range()
-        if start > end: start, end = end, start
-        if not (0 <= start < len(self.ram) and end < len(self.ram)):
-            self.write(f"Invalid memory address")
-            return True
-        for addr in range(start, end+1):
-            self.write(f"{addr:06x}: {self.ram[addr]:06x}")
+    def _cargs_to_addresses(self, cargs):
+        """Checks if arguments are valid memory addresses and returns them as sorted list of integers.
 
+        Args:
+            cargs (str[]): List of strings representing memory addresses
+
+        Returns:
+            bool: True if all arguments are valid memory addresses
+            int[]: Sorted list of integers representing memory addresses
+        """         
+        for i, arg in enumerate(cargs):
+            if not arg.isdigit():
+                return False, []
+            cargs[i] = int(arg)
+            if not (0 <= cargs[i] < len(self.ram)):
+                return False, []
+        if len(cargs) >= 2 and cargs[0] == cargs[1]:
+            return False, []
+        return True, sorted(cargs)
+    
     # [memory]
     def _do_memory(self, addr, *cargs):
         if len(cargs) == 0: self.show()
-        if len(cargs) == 1: self.show_memory(int(cargs[0]))
-        if len(cargs) == 2: self.show_memory(int(cargs[0]), int(cargs[1]))
+        else:
+            start = cargs[0]
+            end = cargs[1] if len(cargs) > 1 else start
+            
+            for addr in range(start, end+1):
+                self.write(f"{addr:06x}: {self.ram[addr]:06x}")
         
         return True
     # [/memory]
     
+
+    # [add]
+    def _do_add_breakpoint(self, addr, *cargs):
+        if cargs: addr = cargs[0]
+        return super()._do_add_breakpoint(addr)
+    # [/add]
+
+    # [clear]
+    def _do_clear_breakpoint(self, addr, *cargs):
+        if cargs: addr = cargs[0]
+        return super()._do_clear_breakpoint(addr)
+    # [/clear]
 
 if __name__ == "__main__":
     VirtualMachine4solved.main()
